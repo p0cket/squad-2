423d4c75adaa96372a14cd01682c42f367baf865
pocket <p0ck31@gmail.com>
2024-11-26 01:02:31 -0500

    fix: finally main ts issues gone

--- src/utils/moves/calculateDamageAndStatuses.ts
+++ src/utils/moves/calculateDamageAndStatuses.ts
@@ -2,29 +2,9 @@ import { AttackPayload } from "../../consts/types"
 import { calcAttack, findRelevantProcs } from "./attackUtils"
 
 export const calculateDamageAndStatuses = (
-    // attacker,
-    // target,
-    // isPlayerAttack,
-    // playerCreatureControlsRef,
-    // enemyCreatureControlsRef,
-    // attack,
-    // dispatch,
-    // playerCreatures,
-    // computerCreatures
     attackPayload: AttackPayload
   ) => {
     console.log(`calculateDamageAndStatuses: attackPayload`, attackPayload)
-    // const {
-    //   attacker,
-    //   target,
-    //   isPlayerAttack,
-    //   playerCreatureControlsRef,
-    //   enemyCreatureControlsRef,
-    //   attack,
-    //   dispatch,
-    //   playerCreatures,
-    //   computerCreatures,
-    // } = attackPayload
     // findRelevantProcs
     const objAfterImmediateStatuses = findRelevantProcs(
       attackPayload,
--- src/utils/moves/handleConfirmedAttack.ts
+++ src/utils/moves/handleConfirmedAttack.ts
@@ -3,157 +3,9 @@ import { AttackPayload, Creature } from "../../consts/types"
 import { processEndOfTurn } from "../turn/processEndOfTurn"
 import { newPerformAttack } from "./performAttack"
 // import { pocketLog } from "../../pocketLog/utils"
-// import { newPerformAttack } from "./performAttack"
 export const getAliveCreatures = (creatures: Creature[]) => {
   return creatures.filter((c) => c.health > 0)
 }
-// export const handleAttack = async (
-//   state,
-//   dispatch,
-//   playerCreatureControlsRef,
-//   enemyCreatureControlsRef
-// ) => {
-//   console.log(
-//     "Handling Attack... State at start:"
-//     // state
-//   )
-//   // just turns + 1
-//   dispatch({ type: "INCREMENT_TURN" })
-
-//   const alivePlayerCreatures = getAliveCreatures(state.playerCreatures)
-//   const aliveComputerCreatures = getAliveCreatures(state.computerCreatures)
-
-//   console.log("Alive player creatures:", alivePlayerCreatures)
-//   console.log("Alive computer creatures:", aliveComputerCreatures)
-
-//   // checking if both player is alive and comp too.
-//   const isPartyDead = checkIfPartyDead(aliveComputerCreatures)
-//   const isPlayerDead = checkIfPartyDead(alivePlayerCreatures)
-//   // but now what do we do with this info?
-
-//   if (alivePlayerCreatures.length > 0 && aliveComputerCreatures.length > 0) {
-//     const playerAttacker = alivePlayerCreatures[0]
-//     const computerTarget = aliveComputerCreatures[0]
-//     console.log(
-//       "Player Attacking computer. Player, target:",
-//       playerAttacker,
-//       computerTarget
-//     )
-//     let damage
-//     try {
-//       damage = await performAttack(
-//         playerAttacker,
-//         computerTarget,
-//         true,
-//         playerCreatureControlsRef,
-//         enemyCreatureControlsRef,
-//         dispatch
-//       )
-//       console.log("Damage dealt by player:", damage)
-//     } catch (error) {
-//       console.error("Error in performAttack:", error)
-//       damage = 0 // Set damage to zero if there's an error to prevent NaN issues
-//     }
-
-//     // error handling
-//     // if (isNaN(damage) || damage === undefined) {
-//     //   console.error(
-//     //     "Error: Damage is NaN or undefined. Player Attacker:",
-//     //     playerAttacker,
-//     //     "Computer Target:",
-//     //     computerTarget
-//     //   )
-//     //   damage = 0 // Prevent health from being affected by invalid damage
-//     // }
-
-//     // lets move this into the performAttack function
-//     const updatedComputerCreature = {
-//       ...computerTarget,
-//       health: Math.max(0, computerTarget.health - damage),
-//     }
-
-//     //update computer with damage
-//     dispatch({
-//       type: "UPDATE_CREATURE",
-//       side: "computerCreatures",
-//       creature: updatedComputerCreature,
-//     })
-
-//     /// ^^^^
-
-//     console.log(
-//       "Computer Target details after attack:",
-//       updatedComputerCreature,
-//       null,
-//       2
-//     )
-//   }
-
-//   if (alivePlayerCreatures.length > 0 && aliveComputerCreatures.length > 0) {
-//     const computerAttacker = aliveComputerCreatures[0]
-//     const playerTarget = alivePlayerCreatures[0]
-
-//     console.log("Computer attacking player...")
-//     console.log("Computer Attacker details:", computerAttacker)
-//     console.log("Player Target details before attack:", playerTarget)
-
-//     let damage
-//     try {
-//       damage = await performAttack(
-//         computerAttacker,
-//         playerTarget,
-//         false,
-//         playerCreatureControlsRef,
-//         enemyCreatureControlsRef,
-//         dispatch
-//       )
-//       console.log("Damage dealt by computer:", damage)
-//     } catch (error) {
-//       console.error("Error in performAttack:", error)
-//       damage = 0 // Set damage to zero if there's an error to prevent NaN issues
-//     }
-
-//     if (isNaN(damage) || damage === undefined) {
-//       console.error(
-//         "Error: Damage is NaN or undefined. Computer Attacker, Player Target:",
-//         computerAttacker,
-//         playerTarget
-//       )
-//       damage = 0 // Prevent health from being affected by invalid damage
-//     }
-
-//     const updatedPlayerCreature = {
-//       ...playerTarget,
-//       health: Math.max(0, playerTarget.health - damage),
-//     }
-
-//     dispatch({
-//       type: "UPDATE_CREATURE",
-//       side: "playerCreatures",
-//       creature: updatedPlayerCreature,
-//     })
-
-//     console.log("Player Target details after attack:", updatedPlayerCreature)
-//   }
-
-//   if (aliveComputerCreatures.length === 0) {
-//     console.log("Player has won the battle. Dispatching WIN_GAME...")
-//     dispatch({ type: "WIN_GAME" })
-//   }
-
-//   const { playerLost, computerLost } = processEndOfTurn(state, dispatch)
-//   console.log(
-//     "End of turn processing results: Player Lost:",
-//     playerLost,
-//     "Computer Lost:",
-//     computerLost
-//   )
-
-//   if (!playerLost && !computerLost) {
-//     console.log("Preparing for next turn...")
-//     dispatch({ type: "PREPARE_NEXT_TURN" })
-//   }
-// }
 // from chooseTargetsModal.js use when someone selects a target and runs the attack
 export const handleTargetedAttack = async (state: any, attackPayload: AttackPayload) => {
   if (!attackPayload) {
@@ -178,15 +30,6 @@ export const handleTargetedAttack = async (state: any, attackPayload: AttackPayl
   // PLAYER ATTACK:
   try {
     await newPerformAttack(
-      // attacker,
-      // selectedTarget,
-      // true, //isPlayer: true so not computer
-      // playerCreatureControlsRef,
-      // enemyCreatureControlsRef,
-      // attack,
-      // dispatch,
-      // state.playerCreatures,
-      // state.computerCreatures
       attackPayload
     )
   } catch (error) {
@@ -203,16 +46,14 @@ export const handleTargetedAttack = async (state: any, attackPayload: AttackPayl
   const aliveComputers = getAliveCreatures(state.computerCreatures)
   const bothTeamsAlive = alivePlayers.length > 0 && aliveComputers.length > 0
   if (bothTeamsAlive) {
-    const computerAttacker = aliveComputers[0]
-    const computersTarget = alivePlayers[0]
-    // const computerAttacker = { comp: 0 }
-    // const computersTarget = { user: 0 }
+    const computerAttacker = aliveComputers[0] //{ comp: 0 }
+    const computersTarget = alivePlayers[0]//{ user: 0 }
     console.log(
       "Computer attacking player: Computer Attacker details & Player Target details before attack:",
       computerAttacker,
       computersTarget
     )
-    const computerAttackPayload = {
+    const computerAttackPayload: AttackPayload = {
       attacker: computerAttacker,
       target: computersTarget, //these should rly be indexes
       playerCreatures: state.playerCreatures,
@@ -226,15 +67,6 @@ export const handleTargetedAttack = async (state: any, attackPayload: AttackPayl
 
     try {
       await newPerformAttack(
-        // computerAttacker,
-        // computersTarget,
-        // false, //isPlayer: false -> so computer
-        // playerCreatureControlsRef,
-        // enemyCreatureControlsRef,
-        // attacks.fireball, // attack. make a comp select a random attack they have
-        // dispatch,
-        // state.playerCreatures,
-        // state.computerCreatures
         computerAttackPayload
       )
     } catch (error) {
@@ -243,12 +75,10 @@ export const handleTargetedAttack = async (state: any, attackPayload: AttackPayl
     }
   }
   //handleEndOfTurnActions
-
   // processEndOfTurn(state, dispatch)
   // applyAfterMoveStatusEffects  // go through and apply poison animation, and any other
   //stacked ones. At the end increase turn by 1.
   processEndOfTurn(state, dispatch)
-
   console.groupEnd()
 }
 
